48b7ed1c4417fd70d524121686f247990c266edd
Camilo López <camiloj.lopez79@gmail.com>
2023-08-06 10:07:18 -0400

    [CONFIG]: Initialized starter project config

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// Next.js
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+
+    // Run the parent , it now includes the custom 
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument;
