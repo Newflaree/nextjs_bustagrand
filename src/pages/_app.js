f18dadb255caf7da6b626437c7bb17147b0e5e09
Camilo López <camiloj.lopez79@gmail.com>
2023-08-06 10:17:33 -0400

    [CONFIG]: Installed Material UI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+// Themes
+import { lightTheme } from '@/themes';
+// Styles
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ lightTheme }>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
